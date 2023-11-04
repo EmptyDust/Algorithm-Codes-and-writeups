c5e6c9a978c3ff023260ec165ee3c5b5e5422dcd
Zhu Kai Nian <1422492074@qq.com>
2023-11-04 17:12:25 +0800

    upd ac785

---
+++ codes/0x20_search/0x21_quick_sort/ac785.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+void quick_sort(vector<int>& list,int pre,int suf){
+    if(pre>=suf)return;
+    int i=pre-1;
+    int j=suf+1;
+    int temp=pre+suf>>1;
+    while(i<j){
+        do i++; while(list[i]<list[temp]);
+        if(i<j) swap(list[i],list[temp]);
+        temp=i;
+        do j--; while(list[j]>list[temp]);
+        if(i<j) swap(list[j],list[temp]);
+        temp=j;
+    }
+    quick_sort(list,pre,i-1);
+    quick_sort(list,i+1,suf);
+}
+int main(){
+    int n;
+    cin>>n;
+    vector<int> list(n);
+    int j=0;
+    for(int i=0;i<n;++i) cin>>list[i];
+    quick_sort(list,0,n-1);
+    for(int num:list) cout<<num<<" ";
+    return 0;
+}
