5e410344e2dd3773ffc45d2082ca134f8a7d01a5
EmptyDust <1422492074@qq.com>
2023-11-14 23:11:57 +0800

    upd ac859

--- codes/0x60_graphs_and_trees/0x63_mfs/ac859.cpp
+++ codes/0x60_graphs_and_trees/0x63_mfs/ac859.cpp
@@ -2,12 +2,11 @@
 #define int long long
 using namespace std;
 
-#define edge struct EDGE 
-struct EDGE
+struct edge
 {
     int u;
     int v;
-    long long w;
+    int w;
 };
 
 signed main(){
@@ -17,28 +16,32 @@ signed main(){
     for(int i=0;i<m;++i){
         int u,v,w;
         cin>>u>>v>>w;
-        mp[i]=(edge){u,v,w};
+        mp[i]={u,v,w};
     }
     sort(mp.begin(),mp.end(),[](edge a,edge b)->bool{
         return a.w<b.w;
     });
-    vector<bool> ds(n);
-    for(int i=0;i<n;++i)ds[i]=i;
+    vector<int> ds(n,0);
+    for(int i=0;i<n;++i)
+        ds[i]=i;
     function<int(int)> get_root = [&](int a)->int{
         return ds[a]=(a==ds[a]?a:get_root(ds[a]));
     };
     int sum=0;
     for(int i=0;i<m;++i){
         edge k = mp[i];
-        if(get_root(k.u)==get_root(k.v))continue;
+        if(get_root(k.u-1)==get_root(k.v-1))
+            continue;
         sum+=k.w;
-        ds[k.u]=k.v;
+        ds[get_root(k.u-1)]=get_root(k.v-1);
     }
-    for(int i=0;i<n;++i)
-        if(ds[i]!=ds[0]){
+    int k = get_root(0);
+    for(int i=1;i<n;++i){
+        if(get_root(i)!=k){
             cout<<"impossible";
             return 0;
         }
+    }
     cout<<sum;
     return 0;
 }
