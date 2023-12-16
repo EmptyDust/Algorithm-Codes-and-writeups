4c4ea3e63256c9a892346f6d3df456aeb7a8d9e6
EmptyDust <1422492074@qq.com>
2023-12-16 21:51:23 +0800

    upd abc333

--- codes/-0x30_competition/-0x34_atcoder/abc333/A.cpp
+++ codes/-0x30_competition/-0x34_atcoder/abc333/A.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 signed main() {
     ios::sync_with_stdio(false);
-
+    int n;cin >> n;
+    for (int i = 0;i < n;++i)
+        cout << n;
     return 0;
 }
--- codes/-0x30_competition/-0x34_atcoder/abc333/D.cpp
+++ codes/-0x30_competition/-0x34_atcoder/abc333/D.cpp
@@ -1,9 +1,30 @@
 #include <bits/stdc++.h>
 #define int long long
 using namespace std;
+constexpr int MAXN = 1e6;
+vector<int> mp[MAXN];
+
+int getnum(int x, int par) {
+    int sum = 1;
+    for (int num : mp[x]) if (num != par) {
+        sum += getnum(num, x);
+    }
+    return sum;
+}
 
 signed main() {
     ios::sync_with_stdio(false);
-
+    int n;cin >> n;
+    for (int i = 0;i < n - 1;++i) {
+        int u, v;cin >> u >> v;
+        u--, v--;
+        mp[u].push_back(v);
+        mp[v].push_back(u);
+    }
+    int ans = 0;
+    for (int num : mp[0]) {
+        ans = max(ans, getnum(num, 0));
+    }
+    cout << n - ans;
     return 0;
 }
