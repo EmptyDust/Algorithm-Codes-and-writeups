48cc229a696f0af3e412a81be6552d4a8e79e91d
EmptyDust <1422492074@qq.com>
2024-05-11 09:32:30 +0800

    upd

--- codes/-0x30_competition/-0x34_atcoder/dp/L.cpp
+++ codes/-0x30_competition/-0x34_atcoder/dp/L.cpp
@@ -1,20 +1,21 @@
 #include <bits/stdc++.h>
-#define int long long 
 using namespace std;
-constexpr int MAXN = 1e3 + 10, mod = 1e9 + 7;
+constexpr int MAXN = 3e3 + 10, mod = 1e9 + 7;
+#define int long long 
 int nums[MAXN];
 int dp[MAXN][MAXN];
 
 signed main() {
-    ios::sync_with_stdio(false);
+    ios::sync_with_stdio(false);cin.tie(0);
     int n;cin >> n;
     for (int i = 1;i <= n;++i)cin >> nums[i];
-    for (int i = 1;i <= n;++i)for (int j = 1;j <= n;++j)if (i + j <= n) {
-        if ((i + j) & 1)
-            dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + nums[i];
+    for (int len = 1;len <= n;++len)for (int i = 1;i + len - 1 <= n;++i) {
+        int j = i + len - 1;
+        if ((n - len) & 1)
+            dp[i][j] = min(dp[i + 1][j] - nums[i], dp[i][j - 1] - nums[j]);
         else
-            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]) + nums[i];
+            dp[i][j] = max(dp[i + 1][j] + nums[i], dp[i][j - 1] + nums[j]);
     }
-
+    std::cout << dp[1][n];
     return 0;
 }
